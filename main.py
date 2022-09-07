c0b3ee98a7b92f43efe8881da3ec96c8d2b76710
lorenzobonomi <lorenzobonomi@hotmail.com>
2022-09-07 09:22:55 -0600

    First app main file created

---
+++ main.py
@@ -0,0 +1,18 @@
+## Modules for project
+
+# Standard modules
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+
+## App
+
+# Data load from database
+
+# Streamlit layout main parameters
+st.set_page_config(layout = 'wide')
+st.title('Sof')
+st.subheader('')
+
+
